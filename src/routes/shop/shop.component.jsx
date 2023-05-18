5579b2c6a0a25f0c93523fb2d9369614fe788448
Caleb Cox <calebcox2010@gmail.com>
2023-05-17 22:54:00 -0400

    Add CategoriesPreview route component

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,23 +1,15 @@
-import { useContext } from "react";
+import { Routes, Route } from 'react-router-dom';
 
-import CategoryPreview from "../../components/category-preview/category-preview.component";
-
-import { CategoriesContext } from "../../contexts/categories.context";
+import CategoriesPreview from '../categories-preview/categories-preview.component';
 
 import '../shop/shop.styles.scss'
 
 const Shop = () => {
-    const {categoriesMap} = useContext(CategoriesContext);
     
-    return(
-        <div className="shop-container">
-        {
-            Object.keys(categoriesMap).map(title => {
-                const products = categoriesMap[title];
-                return <CategoryPreview key={title} title={title} products={products} />
-            })
-        }
-        </div>
+    return (
+        <Routes>
+            <Route index element={<CategoriesPreview />} />
+        </Routes>
     )
 }
 
